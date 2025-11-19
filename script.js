0054ff3e27fda6f109f2d0d9ea6fb59e10a2be24
SANDHUMODEL <sandhumodel1313@gmail.com>
2025-11-19 12:15:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // --- START: Latest mods data (edit filenames if needed) ---
 const mods = [
-  { title: "Swaraj 963 Simple for Farming", img: "images/tractor1.png", tag: "PAID" },
+  { title: "KATKAT TRACTOR JONI BHAI FS20", img: "images/tractor1.png", tag: "PAID" },
   { title: "Swaraj 963 Custom for Farming", img: "images/tractor2.png", tag: "PAID" },
   { title: "Small Loader Mod",                img: "images/tractor3.png", tag: "FREE" },
   { title: "John Deere 3650 Super",           img: "images/tractor1.png", tag: "PAID" }
